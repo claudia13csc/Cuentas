0f1e574bbe7c077a18375bf2f9830d7bebeffb02
claudia13csc <claudixsum@gmail.com>
2017-06-05 23:55:03 -0300

    modificado

--- TP2/src/ar/edu/unlam/CuentaCorriente.java
+++ TP2/src/ar/edu/unlam/CuentaCorriente.java
@@ -1,41 +1,43 @@
 package ar.edu.unlam;
 /*Cuenta Corriente
-La más compleja de las cuentas, ésta permite establecer una cantidad de dinero a girar en descubierto. 
-Es por ello que cada vez que se desee extraer dinero, no sólo se considera el que se posee, 
-sino el límite adicional que el banco estará brindando. Por supuesto esto no es gratis, ya que el banco
- nos cobrará un 5% como comisión sobre todo el monto en descubierto consumido en la operación. 
- Por ejemplo, si tuviéramos $ 100 en la cuenta, y quisiéramos retirar $ 200 (con un descubierto de $ 150),
-  podremos hacerlo. Pasaremos a deberle al banco $ 105 en total: los $ 100 que nos cubrió,
-   más el 5% adicional sobre el descubierto otorgado.*/
+La mÃ¡s compleja de las cuentas, Ã©sta permite establecer una cantidad de dinero a girar en descubierto. 
+Es por ello que cada vez que se desee extraer dinero, no sÃ³lo se considera el que se posee, 
+sino el lÃ­mite adicional que el banco estarÃ¡ brindando. Por supuesto esto no es gratis, ya que el banco
+ nos cobrarÃ¡ un 5% como comisiÃ³n sobre todo el monto en descubierto consumido en la operaciÃ³n. 
+ Por ejemplo, si tuviÃ©ramos $ 100 en la cuenta, y quisiÃ©ramos retirar $ 200 (con un descubierto de $ 150),
+  podremos hacerlo. Pasaremos a deberle al banco $ 105 en total: los $ 100 que nos cubriÃ³,
+   mÃ¡s el 5% adicional sobre el descubierto otorgado.*/
 
 public class CuentaCorriente extends CuentaSueldo {
 	private double girarDescubierto;
 	private  double comisionBanco;
 	//private double deudaConBanco;
 	
-	public CuentaCorriente (double total,double cantidadExtraParaGirar)
+	public CuentaCorriente (Double cuenta,Double cantidadExtraParaGirar )
 	{
-		super(total);
-		this.girarDescubierto=cantidadExtraParaGirar;
-		this.comisionBanco= 0.05;
-	
+		super(cuenta);
+		this.girarDescubierto = cantidadExtraParaGirar;
+		this.comisionBanco = 0.05;
+		this.deudaConBanco = 0.00;
 	}
 	
 	
-	public double getGirarDescubierto(double dineroRetirar)
+	
+	
+	public void setPosibleExtraerExtra (Double retiroDinero)
 	{
-		if(dineroRetirar>cuenta)
-		{
-			dineroRetirar=dineroRetirar - cuenta;
-			
-			if(girarDescubierto>dineroRetirar)
-			{
-				return dineroRetirar*comisionBanco;
-				
-			}
-			
-		}
-	return cuenta;	
+		 if( retiroDinero > cuenta){
+			 retiroDinero= retiroDinero-cuenta;
+			if(retiroDinero<=girarDescubierto)
+			 deudaConBanco=retiroDinero*comisionBanco;
+		 }
+		
+	}
+	
+	public Double deudaBanco()
+	{
+		
+		return deudaConBanco;
 	}
 	
 	
